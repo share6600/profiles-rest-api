35e123153670a3b0371b8fa39e3fc3ea65af3fb1
sharif <sharifmostafa1122@gmail.com>
2020-01-10 01:21:13 +0300

    add retreve update partial_update destroy to apiviewset and configure the urls for apiviewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 
 from profiles_api import views
 
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
