2ae0010b31fb1977cf265dd1968fc151b42a88c7
sharif <sharifmostafa1122@gmail.com>
2020-01-10 02:47:45 +0300

    register profile viewset with url router

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,10 +5,11 @@ from profiles_api import views
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
-
+from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile',views.UserProfilesViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
